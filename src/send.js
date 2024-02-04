cd51430b5b42f61503c215b9dd245ee08278e8ab
Juhee Kang <38813640+juheesvt@users.noreply.github.com>
2024-02-04 21:46:50 +0900

    feat: 2차 파일럿 프로그램 개발 기간 공지

--- src/send.js
+++ src/send.js
@@ -5,8 +5,14 @@ const hook = new Webhook(process.env.WEBHOOK_URL);
 
 async function sendMessage() {
   const embed = new MessageBuilder()
-    .setTitle("❤️ 오늘 하루도 화이팅 ❤️")
-    .setColor("#00ff00")
+    .setTitle("❤️ 만나팀 모두 오늘 하루도 화이팅 ❤️")
+    .setColor("#ffd400")
+    .setDescription(
+      "지금은 2차 파일럿 프로그램 개발 기간입니다."
+    )
+    .addField(
+      "✅ 액션아이템 채널에 [우리 팀원들이 몽글에 담고 싶은 가치]를 생각해서 적어주세요 !"
+    );
 
   hook.send(embed);
 }
